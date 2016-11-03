b86d91c6090482aa726aec62e44a56c9faa1e4e1
Gabriel Breiner <a1447920@unet.univie.ac.at>
2016-11-03 16:57:09 +0100

    Server.java edit

--- src/Sockets/Server.java
+++ src/Sockets/Server.java
@@ -1,10 +1,23 @@
-public Class Server extends Thread {
+package Sockets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class Server extends Thread {
 
         public static final int NTHREADS = 5;
-        ExecutorService executor = new ExecutorService(NTHREADS);
+        private static final int SERVERPORT = 1666;
+        private ServerSocket serverSocket = null;
+        private Socket clientSocket = null;
+        private ExecutorService executor = Executors.newFixedThreadPool(NTHREADS);
 
         public static void main( String[] args ){
-                System.out.println("Dafuq");
+
+                System.out.println("CIAO");
                 System.exit(1);
         }
+
+        public void run(){}
+        public void init(){}
 }
